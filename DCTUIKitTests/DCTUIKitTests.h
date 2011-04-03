c62f56df70b99d7b3c86497fe9a875a7cac16c68
Daniel Tull <dt@danieltull.co.uk>
2011-04-03 21:47:34 +0100

    New project to build a library

---
+++ DCTUIKitTests/DCTUIKitTests.h
@@ -0,0 +1,17 @@
+//
+//  DCTUIKitTests.h
+//  DCTUIKitTests
+//
+//  Created by Daniel Tull on 3.04.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DCTUIKitTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ DCTUIKitTests/DCTUIKitTests.m
@@ -0,0 +1,33 @@
+//
+//  DCTUIKitTests.m
+//  DCTUIKitTests
+//
+//  Created by Daniel Tull on 3.04.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import "DCTUIKitTests.h"
+
+
+@implementation DCTUIKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCTUIKitTests");
+}
+
+@end
