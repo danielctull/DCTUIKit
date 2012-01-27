6cdc7b032271503eafc0a239603115e4bbe43928
Daniel Tull <dt@danieltull.co.uk>
2012-01-27 00:53:29 +0000

    Add version header

---
+++ DCTUIKit.h
@@ -0,0 +1,39 @@
+//
+//  DCTUIKit.h
+//  DCTUIKit
+//
+//  Created by Daniel Tull on 27.01.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#ifndef dctuikit
+
+#define dctuikit_1_0     10000
+#define dctuikit_1_0_1   10001
+#define dctuikit_1_0_2   10002
+#define dctuikit_1_0_3   10003
+#define dctuikit_1_0_4   10004
+
+#define dctuikit_1_1     10100
+#define dctuikit_1_1_4   10104
+#define dctuikit_1_1_5   10105
+#define dctuikit_1_1_6   10106
+#define dctuikit_1_1_7   10107
+#define dctuikit_1_1_8   10108
+
+#define dctuikit_1_2     10200
+#define dctuikit_1_2_1   10201
+#define dctuikit_1_2_2   10202
+#define dctuikit_1_2_3   10203
+#define dctuikit_1_2_4   10204
+#define dctuikit_1_2_5   10205
+
+#define dctuikit_2_0     20000
+#define dctuikit_2_0_1	 20001
+#define dctuikit_2_0_2	 20002
+
+#define dctuikit         dctuikit_2_0_2
+
+#endif
