da7bfab2a4d2a26adcc8b1177782ea300cf76065
Daniel Tull <dt@danieltull.co.uk>
2012-03-12 14:27:52 +0000

    Add category on UIColor to alter the alpha component of colors

---
+++ UIColor+DCTMixing.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+DCTMixing.h
+//  Tweetopolis
+//
+//  Created by Daniel Tull on 12.03.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (DCTMixing)
+
+- (UIColor *)dct_colorWithAlpha:(CGFloat)alpha;
+
+@end
